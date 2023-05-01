44ec7af278996d2742163f253eed1f7afb22c060
Sahil Darunde <sahildarunde007@gmail.com>
2023-05-01 18:05:16 +0530

    All tasks done

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -2,12 +2,8 @@ package com.driver.test;
 
 import static org.junit.Assert.*;
 
-import com.driver.B;
+
 import org.junit.jupiter.api.Test;
 
 public class TestCases {
-    public static void main(String args[]){
-      B obj = new B();
-      obj.meth();
-   }
 }
