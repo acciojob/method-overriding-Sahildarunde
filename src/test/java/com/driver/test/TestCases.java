23bfd18ad1965f93719c8d3c6073805a3a4a884c
Sahil Darunde <sahildarunde007@gmail.com>
2023-05-01 17:50:20 +0530

    rewritten code

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -1,8 +1,13 @@
 package com.driver.test;
 
 import static org.junit.Assert.*;
+
+import com.driver.B;
 import org.junit.jupiter.api.Test;
 
 public class TestCases {
-    
+    public static void main(String args[]){
+      B obj = new B();
+      obj.meth();
+   }
 }
